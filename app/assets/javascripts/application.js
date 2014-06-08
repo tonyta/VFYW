4ec1cc14c410f9c80f9bd3249c9800562f7f60ac
Tony Ta <tonyta.tt@gmail.com>
2014-06-08 16:06:12 -0700

    sets up initial layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,53 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+
+// SLIDERS
+
+  // timeSlider.prototype = {
+  //   initialize: function(cssSelector) {},
+  //   convert12Hour: function(hour) {},
+  //   getTimeStr: function(start, end) {}
+  // }
+
+  var getTime = function(hour) {
+    if (hour === 0 || hour === 24) {
+      return "12 am";
+    } else if (hour === 12) {
+      return "12 pm";
+    } else if (hour > 11) {
+      return (hour - 12) + " pm";
+    } else {
+      return hour + " am";
+    }
+  };
+
+  var initialTimeRange = [8, 12];
+
+  $( "#time-range" ).slider({
+    range: true,
+    min: 0,
+    max: 24,
+    values: initialTimeRange,
+    slide: function( event, ui ) {
+      $( "#time" ).text(getTime(ui.values[0]) + " to " + getTime(ui.values[1]));
+    },
+    stop: function( event, ui ) {
+      console.log('stopped');
+    }
+  });
+
+  $("#time").text(getTime(initialTimeRange[0]) +
+    " to " + getTime(initialTimeRange[1]) );
+
+
+// MAP
+
+  var map = L.mapbox.map('map', 'tonyta.if74jkhj')
+  map.setView([30,11], 2);
+
+  window.map = map;
+
+})
